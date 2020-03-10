ff0b5a90fd6b0b17209720e312c66365bebf5d83
Ivan Diaz <ivanid22@gmail.com>
2020-03-10 19:44:17 -0300

    Add class CVEEntry

---
+++ bin/nvd-scrape.rb
@@ -0,0 +1,20 @@
+require 'nokogiri'
+require 'open-uri'
+require './lib/cve_entry'
+
+doc = Nokogiri::XML(open('https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml'))
+
+doc.remove_namespaces!
+entries = []
+
+doc.search('item').each do |item|
+  entries.push(CVEEntry.new(item.children[1].child.content, item.children[3].child.content, item.children[5].child.content, item.children[7].child.content) )
+  print "title: #{item.children[1].child.content}\n"
+  print "link: #{item.children[3].child.content}\n"
+  print "description: #{item.children[5].child.content}\n"
+  print "date: #{item.children[7].child.content}\n"
+  puts
+end
+
+entries.last.establish_severity
+
